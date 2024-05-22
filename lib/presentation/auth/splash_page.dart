1d58c5c1232406177234240566ca8a34abaf2f27
RendyCahyaE <rendycahya6@gmail.com>
2024-05-22 13:16:29 +0700

    add assets init

---
+++ lib/presentation/auth/splash_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wisata_app/core/core.dart';
+import 'package:flutter_wisata_app/presentation/auth/login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => context.pushReplacement(const LoginPage()),
+    );
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(96.0),
+        child: Center(
+          child: Assets.images.logoBlue.image(),
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 100.0,
+        child: Align(
+          alignment: Alignment.center,
+          child: Assets.images.logoCwb.image(width: 96.0),
+        ),
+      ),
+    );
+  }
+}
